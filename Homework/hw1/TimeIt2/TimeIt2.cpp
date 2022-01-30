88575b0472b2ba0fed5c969cc004d80ffdeaa2c0
Bernard Laughlin <bernardlaughlin@gmail.com>
2022-01-30 15:58:31 -0800

    added readInBook() to TimeIt2.cpp

--- Homework/hw1/TimeIt2/TimeIt2.cpp
+++ Homework/hw1/TimeIt2/TimeIt2.cpp
@@ -6,7 +6,42 @@
 using std::cout;
 using std::endl;
 #include "StopWatch.hpp"
+#include <vector>
+using std::vector;
+using std::string;
+#include <string>
+using std::string;
+using std::getline;
+#include <fstream>
+using std::ifstream;
+#include <random>
+using std::random_device;
+using std::mt19937;
+#include <algorithm>
 
+// Reads in lines from book and returns them as a vector
+vector<string> readInBook(string filename){
+    vector<string> book;
+    ifstream fin(filename);
+    if(!fin){
+        cout << "Error reading file." << endl;
+    } else {
+        while (true){
+            string line;
+            getline(fin, line);
+            if (!fin) {
+                if (fin.eof()) {
+                    cout << "Finished reading file." << endl;
+                } else {
+                    cout << "Error during transmit." << endl;
+                }
+                break;
+            }
+            book.push_back(line);
+        }
+    }
+    return book;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
