557c836c93ae76d2705ad52c1c2acb002cc6d7fd
Bernard Laughlin <bernardlaughlin@gmail.com>
2022-02-20 13:28:36 -0800

    first commit of lowercase.cpp

---
+++ Homework/hw2/LowerCase.cpp/cmake-build-debug/CMakeFiles/LowerCase_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LowerCase_cpp.dir/lowercase.cpp.o"
+  "LowerCase_cpp"
+  "LowerCase_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LowerCase_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Homework/hw2/LowerCase.cpp/lowercase.cpp
@@ -0,0 +1,13 @@
+// lowercase.cpp
+// Bernard Laughlin 2/20/22
+// Converts upper case to lower case
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
