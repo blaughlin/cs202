f7e7f8b0932cc7f83ef8e9e02959ce492cec2f2b
Bernard Laughlin <bernardlaughlin@gmail.com>
2022-04-07 11:19:50 -0700

    creatred FilledBox derived class

--- Homework/hw4/boxes/main.cpp
+++ Homework/hw4/boxes/main.cpp
@@ -3,12 +3,16 @@
 // Homework 4 boxes assignment for cs202
 
 #include <iostream>
+using std::endl;
+using std::cout;
 using std::ostream;
 #include <string>
 using std::string;
+#include <fstream>
 
 class Box {
 public:
+    Box(int w, int h) : _width(w), _height(h) {}
     int getWidth() {return _width;}
     int getHeight() {return _height;}
     void setWidth(int w) {_width = w;}
@@ -26,7 +30,30 @@ ostream & operator<<( ostream &os, Box &b){
     return os;
 }
 
+class FilledBox : public Box {
+public:
+    FilledBox() : Box(1, 1) {}
+    FilledBox(int w, int h) : Box(w,h) {}
+    virtual string type() {return "FilledBox";}
+    virtual void print (ostream &os) {
+        int w = getWidth();
+        for (int i = 0; i <= getHeight() -1; i++){
+            for (auto j = 0; j <= getWidth() -1; j++){
+                os << "x";
+            }
+            os << endl;
+        }
+    }
+
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ofstream stream("test.txt");
+    FilledBox a;
+    FilledBox  b(3,4) ;
+    std::cout << "Hello, World! " <<  a.type() << ", width: " << a.getWidth() << std::endl;
+    b.print(stream);
+
+    cout << b << endl;
     return 0;
 }
