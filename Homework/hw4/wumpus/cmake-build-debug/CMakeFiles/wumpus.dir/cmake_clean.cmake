892a8fa4622bb9e3af5f67788f2b2164535a7b43
Bernard Laughlin <bernardlaughlin@gmail.com>
2022-04-10 08:18:06 -0700

    added Cave struct

---
+++ Homework/hw4/wumpus/cmake-build-debug/CMakeFiles/wumpus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wumpus.dir/main.cpp.o"
+  "wumpus"
+  "wumpus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wumpus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Homework/hw4/wumpus/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+#include <vector>
+using std::vector;
+#include <random>
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+#include<algorithm>
+using std::find;
+
+// create game class
+// create room struct that has connecting room vector, contents(player, bat, wumpus)
+// create function to make map that has a parameter number of room to make, need to randomly chose N, S
+// E, W for 3 connecting rooms, create a loop that fills up a cave vector and stops when room count parameter is
+// equal to cave count.
+// randomly assign were wumpus and bat is
+// create a function to shoot and check if wumpus will be killed if not killed move wumpus
+// create method to display text of what is in the room and options where to go
+// create a method to get user input
+
+struct Cave {
+    int id;
+    vector<Cave> connectingRooms;
+    bool hasPlayer = false;
+    bool hasBat = false;
+    bool hasWumpus = false;
+    Cave* north = NULL;
+    Cave* south = NULL;
+    Cave* east = NULL;
+    Cave* west = NULL;
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
